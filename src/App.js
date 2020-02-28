cdf1b0bc799dd7648172a829f4511fc294bc0eaa
Juni Yadi <juniyadi94@gmail.com>
2020-02-28 21:07:28 +0700

    Update Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./styles.css";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
